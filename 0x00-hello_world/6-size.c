6941c3fadf7f364df43feeced93d415be91e7cfa
EimanEltayeb <em_moh092@hotmail.com>
2023-09-07 19:29:38 -0700

    g

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,10 @@ int main(void)
 {
 	char c;
 	int i;
-	double d;
+	float f;
 
-	printf("Size of a char: 1 byte(s)
+	printf("Size of a char: 1 byte(s);
+	printf("Size of an int: 1 byte(s);
+	printf("Size of a long int: 1 byte(s)
+	printf("Size of a long long int: 1 byte(s)
+	printf("Size of a float: 1 byte(s)
