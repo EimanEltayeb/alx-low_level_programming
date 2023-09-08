b3694c5df80f8cede78ac91d697d54d63ea5deae
EimanEltayeb <em_moh092@hotmail.com>
2023-09-08 15:48:19 -0700

    f

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/*prints all single digit numbers of base 10 starting from 0*/
+int main(void)
+{
+	char  i = 0;
+
+	while (i < 10)
+	{
+		putchar(i);
+		i = i + 1;
+	}
+	return (0);
+}
