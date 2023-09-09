d641ae07739770eae506c586bd11fa6293ef30c2
EimanEltayeb <em_moh092@hotmail.com>
2023-09-09 15:16:48 -0700

    g

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: a program to print a specific text
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+
+	return (1);
+}
