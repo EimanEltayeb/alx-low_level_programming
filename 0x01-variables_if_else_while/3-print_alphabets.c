b9845dd03f41153030a69f35d42a672a5f3ecb3f
EimanEltayeb <em_moh092@hotmail.com>
2023-09-08 18:17:34 -0700

    g

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/*
+ * main = Entry function
+ * Return = 0
+ */
+int main(void);
+{
+	int Alpha = "a";
+	int Alphabit = "A";
+
+	while (Alpha <= "z")
+	{
+		putchar(Alpha);
+	}
+
+	while (Alphabit <= "Z")
+	{
+		putchat(Alphabit);
+	}
+
+	putchar('n\');
+
+	return (0);
+}
