24857111ccd72140fdf451949fbc773f3495368d
EimanEltayeb <em_moh092@hotmail.com>
2023-09-12 15:16:24 -0700

    gdsfgr

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Description: a program to print alphabet
+ *
+ * Return: 0
+ */
+void print_alphabet(void)
+{
+	char alpha = a;
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+		alpha++;
+	}
+	_putchar("\n");
+
+	return (0);
+}
