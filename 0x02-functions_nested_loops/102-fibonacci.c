20fe05b7ceab47182b602322fa8cc166de3ae627
EimanEltayeb <em_moh092@hotmail.com>
2023-09-13 16:43:23 -0700

    g

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * main - a function to print the Fibonacci numbers.
+ * Return: 0
+ */
+int main(void)
+{
+	int i, j, x, sum;
+
+	i = 1;
+	j = 2;
+	sum = i + j;
+	for (x = 0; x < 50; x++)
+	{
+		printf("%d\n", i);
+		i = j;
+		j = sum;
+	}
+	return (0);
+}
