1ed4b234788d353b740af778426209027bdfd20c
EimanEltayeb <em_moh092@hotmail.com>
2023-09-13 18:47:32 -0700

    g

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,6 +5,23 @@
  */
 int main(void)
 {
-	int i, j;
+	float i;
+	unsigned long f1, f2, sum;
 
-		
+	f1 = 0;
+	f2 = 1;
+
+	while (1)
+	{
+		sum = f1 + f2;
+		if (sum > 4000000)
+			break;
+
+		if ((sum % 2 == 0))
+			i += sum;
+		f1 = f2;
+		f2 = sum;
+	}
+	printf("%f\n", i);
+	return (0);
+}
