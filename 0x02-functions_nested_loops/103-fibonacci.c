a37466711f4009a4e3368795eb8ac904cb78c7e3
EimanEltayeb <em_moh092@hotmail.com>
2023-09-13 17:13:06 -0700

    n

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * main - print the sum of even valued fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+	
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+/**
+ * main - finds and prints the first 98 Fibonacci numbers
+ * Return: 0
+ */
+int main(void)
+{
+
