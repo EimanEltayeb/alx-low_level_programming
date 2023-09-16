9916cf6a629f19f8c58d7442f840147317fd60c5
EimanEltayeb <em_moh092@hotmail.com>
2023-09-16 10:23:21 -0700

    l

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<errno.h>
 /**
  * main - Entry point
  *
@@ -8,6 +9,11 @@
  */
 int main(void)
 {
+	FILE f;
+
+	f = fopen("dfnvdfviunf", "fvnj");
+
+	if (f == NULL) 
 	perror("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	return (1);
