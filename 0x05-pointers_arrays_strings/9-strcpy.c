c001be8a0bbe270d247eeed90b04bb30d4e71613
EimanEltayeb <em_moh092@hotmail.com>
2023-09-18 19:55:16 -0700

    g

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ * *_strcpy - function that copies the string pointed to by src
+ * @dest: buffer pointer
+ * @src: input string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int size = 0;
+
+	while (srt[size] != 0)
+		size++
+	
