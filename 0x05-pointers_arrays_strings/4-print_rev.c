195a8b6f5dbb8ef1f443ba6253cd27ca849d03f3
EimanEltayeb <em_moh092@hotmail.com>
2023-09-18 18:09:24 -0700

    g

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,11 @@
+#include"main.h"
+/**
+ * print_rev - a function that prints a string, in reverse
+ * @s string
+ */
+void print_rev(char *s)
+{
+	int i, n;
+
+	for (i = n; i >= 0; i--)
+		
---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include"main.h"
+/**
+ *puts_half - function that prints half of a string
+ @str: the string
+ */
+void puts_half(char *str)
+{
+	int i;
+	int n;
+
+	if 
+	for (i = 0; i <= (n / 2); i++)
+
