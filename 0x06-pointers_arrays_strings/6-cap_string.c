c3f3434d2fb68b1768367a4afc349b2dec87768c
EimanEltayeb <em_moh092@hotmail.com>
2023-09-21 17:41:46 -0700

    g

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -6,16 +6,35 @@
  */
 char *cap_string(char *s)
 {
-	int i;
-
+	int i, j;
+	char x[12];
 	int size;
+
+	x[0] = ' ';
+	x[1] = '\n';
+	x[2] = ',';
+	x[3] = ';';
+	x[4] = '.';
+	x[5] = '!';
+	x[6] = '?';
+	x[7] = '"';
+	x[8] = '(';
+	x[9] = ')';
+	x[10] = '{';
+	x[11] = '}';
 
 	while (s[size] != 0)
 		size++;
 	for (i = 0; i < size; i++)
 	{
-		if ((i == 0 || s[i - 1] == ' ') && (s[i] <= 'z' && s[i] >= 'a'))
-			s[i] = s[i] - 32;
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			for (j = 0; j < 12; j++)
+			{
+				if (s[i - 1] == x[j])
+					s[i] = s[i] - 32;
+			}
+		}
 	}
 	return (s);
 }
