c480a1a6385a090bc2549d85c92fa7e713a1bf90
EimanEltayeb <em_moh092@hotmail.com>
2023-09-26 17:09:38 -0700

    g

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -12,7 +12,7 @@ char *_strpbrk(char *s, char *accept)
 
 	for (i = 0; s[i] != 0; i++)
 	{
-		for (j = 0; accept[j] != s[i]; j++)
+		for (j = 0; accept[j] != 0; j++)
 		{
 			if (accept[j] == s[i])
 				p = &(s[i]);
