91793d4d6eee6eea756e42505e5fdd7f5c2505dc
EimanEltayeb <em_moh092@hotmail.com>
2023-09-28 13:43:21 -0700

    f

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion - a function that return the velue of x to teh power y
+ * @x: base num
+ * @y: power num
+ * Return: x to the power y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y == 0)
+		return (1);
+	if (y < 0)
+		return (-1);
+	 return (x * (_pow_recursion(x, y - 1)));
+}
