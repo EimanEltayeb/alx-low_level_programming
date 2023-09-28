cc37c38bd658cf5ce2688d230544f04ebba70d1a
EimanEltayeb <em_moh092@hotmail.com>
2023-09-27 21:32:30 -0700

    atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ *_atoi - function that convert a string to an integer.
+ * @s: input char
+ * Return: int
+ */
+int _atoi(char *s)
+{
+	int i = 0;
+	int num = 0;
+	int sign = 1;
+
+	{
+		while (s[i] != '\0')
+		{
+			if (s[i] >= 48 && s[i] <= 57)
+				num = num * 10 + (s[i] - '0');
+			else if (s[i] == '-')
+				sign *= -1;
+			if (num > 0 && s[i] == ' ')
+				break;
+			i++;
+		}
+		return (num * sign);
+	}
+}
---
+++ 0x05-pointers_arrays_strings/demo.c
@@ -0,0 +1,8 @@
+#include "main.h"
+/**
+ * avg - calculates the average of two numbers
+ * @n1: first number
+ * @n2: second number
+ */
+void  avg(void)
+{:
