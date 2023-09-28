84baa191de116dd1155e95358a8c7b4116affcfc
EimanEltayeb <em_moh092@hotmail.com>
2023-09-28 15:34:19 -0700

    d

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,7 +5,11 @@
  */
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	_putchar(s[i]);
-	s[i]
+	if (*s == '\0')
+		_putchar('\n');
+	else
+	{
+		_putchar(*s);
+		_puts_recursion((s + 1));
+	}
+}
--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,4 +5,7 @@
  */
 void _print_rev_recursion(char *s)
 {
+	int i;
 	
+	_print_rev_recursion(s + 1)
+	_putchar(s	
