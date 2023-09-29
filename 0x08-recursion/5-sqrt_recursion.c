21b4d652cfe997f88047c23e1f308bd4d093b9ce
EimanEltayeb <em_moh092@hotmail.com>
2023-09-28 17:33:40 -0700

    g

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _sqrt_recursion - a function returns the natural sqrt of a number.
+ * @n: input num
+ * return: sqrt or -1
+ */
+int _sqrt_recursion(int n)
+{
+	int i = 1;
+	int sqrt;
+
+	if (
+
+			_sqrt_recursion(n)
+}
+#include "main.h"
+{
+	
+ printf("%i",	__sqrt_recursion(8));
+}
+
