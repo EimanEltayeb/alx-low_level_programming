79d39faa69c0f82ff2c7c7ff946e444dd85b6a17
EimanEltayeb <em_moh092@hotmail.com>
2023-09-28 18:48:10 -0700

    f

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -2,15 +2,26 @@
 /**
  * is_prime_number - a function to know if the number is prime
  * @n: input num
+ * @x: support variable
  * Return: 0 or 1
  */
+int prime_check(int n, int x);
 int is_prime_number(int n)
 {
-	int check;
-	int x = n - 1;
-
-	check = n % x;
-	if (check == 0)
-		return (0);
+	return (prime_check(n, 2));
+}
+/**
+ * prime_check - is a support function
+ * @n: input number
+ * @x: support variable
+ * Return: 0 or 1
+ */
+int prime_check(int n, int x)
+{
+	if (x == n)
+		return (1);
+	else if (x < n && n % x != 0)
+		return (prime_check(n, x + 1));
 	else
-		is_prime
+		return (0);
+}
