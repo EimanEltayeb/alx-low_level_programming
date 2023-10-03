bef83b036533b69cd13160b3cd7c6804afbf6823
EimanEltayeb <em_moh092@hotmail.com>
2023-10-03 12:44:04 -0700

    g

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - a program to print its name
+ *@argv: arguments
+ *Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	 printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *@argc: argument number
+ *@argv[]: arguments
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
