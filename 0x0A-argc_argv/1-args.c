62da5fe96d358e2a2498612abe56ddb32c983ee2
EimanEltayeb <em_moh092@hotmail.com>
2023-10-03 12:52:51 -0700

    f

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,9 +3,11 @@
  * main - main function
  *@argc: argument number
  *@argv[]: arguments
+ *return: 0;
  */
 int main(int argc, char *argv[])
 {
+	(void)argv;
 	printf("%d\n", argc);
 	return (0);
 }
