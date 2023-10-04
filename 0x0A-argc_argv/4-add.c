c4dec428a23a89568ab2c3c640eb5892f82bd8da
EimanEltayeb <em_moh092@hotmail.com>
2023-10-03 17:10:38 -0700

    g

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -8,14 +8,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, j, result = 0;
+	int i, j, k, result = 0;
 
 	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < 47 || *argv[i] > 57)
+		for (k = 0; argv[i][k] != '\0'; k++)
 		{
-			printf("Error\n");
-			return (1);
+			if (*argv[i] < '0' || *argv[i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
 	}
 	for (j = 1; j < argc; j++)
