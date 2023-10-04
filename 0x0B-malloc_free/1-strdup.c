0335ad699b84c5467b2ec4718630dbd28c07bf7d
EimanEltayeb <em_moh092@hotmail.com>
2023-10-04 15:39:40 -0700

    g

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -14,10 +14,10 @@ char *_strdup(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 		size++;
 	s = malloc(sizeof(char) * (size + 1));
+	if (s == NULL)
+		return (NULL);
 	for (i = 0; str[i] != '\0'; i++)
 		s[i] = str[i];
 	s[size] = '\0';
-	if (s == NULL)
-		return (NULL);
 	return (s);
 }
