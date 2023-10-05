7cf3fa2abab7dd1fdc084f4d4be59c4bf9fe1a5d
EimanEltayeb <em_moh092@hotmail.com>
2023-10-04 18:40:43 -0700

    gdf

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,6 +18,13 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 	{
 		s[i] = malloc(width * sizeof(int));
+		if (s[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+				free(s[j]);
+			free(s);
+			return(NULL);
+		}
 	}
 	for (i = 0; i < height; i++)
 	{
