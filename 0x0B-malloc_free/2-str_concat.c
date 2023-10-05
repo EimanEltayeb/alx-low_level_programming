da9607c194bdcabfb2f2dc666473350617463707
EimanEltayeb <em_moh092@hotmail.com>
2023-10-04 19:42:09 -0700

    g

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -18,16 +18,14 @@ char *str_concat(char *s1, char *s2)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	s = malloc(sizeof(char) * (size1 + size2));
+	s = malloc(sizeof(char) * (size1 + size2 + 1));
 	if (s == NULL)
 		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];
-	for (i = size1; i < (size1 + size2 - 1); i++)
-		s[i] = s2[i - size1];
-	for (i = 0; s[i] != '\0'; i++)
-		s[i] = s[i];
-	s[(size1 + size2 - 1)] = '\0';
+	for (i = 0; i < size2; i++)
+		s[size1 + i] = s2[i];
+	s[(size1 + size2)] = '\0';
 	return (s);
 }
