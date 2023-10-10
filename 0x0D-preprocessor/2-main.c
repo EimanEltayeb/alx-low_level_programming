1a64e98172866a67013fd1509fceaeead6667cb0
EimanEltayeb <em_moh092@hotmail.com>
2023-10-10 08:48:44 -0700

    gd

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 /**
  * main - the main program
- * @argc: No of arguments
- * @argv: the arguments array
  * Return: 0 or 1.
  */
-int main(int argc, char *argv[])
+int main(void)
 {
-
+	printf("%s\n", _BASE_FILE_);
+	return (0);
+}
