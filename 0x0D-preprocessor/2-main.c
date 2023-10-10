09b058620c78a74398eab5df7c9c6249eaa5d264
EimanEltayeb <em_moh092@hotmail.com>
2023-10-10 08:27:22 -0700

    sum

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main program
+ * @argc: No of arguments
+ * @argv: the arguments array
+ * Return: 0 or 1.
+ */
+int main(int argc, char *argv[])
+{
+
