eca45429eb23702a1fecbb90d53100e5e8faca83
EimanEltayeb <em_moh092@hotmail.com>
2023-10-11 07:23:14 -0700

    gh

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,16 @@
+#ifndef _3_-_CALC_H
+#define _3_-_CALC_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+
+
+
+#endif
