3c2fa9162f655bec8972fa1e6021cadd66fc6895
EimanEltayeb <em_moh092@hotmail.com>
2023-10-11 13:17:13 -0700

    f

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,1 +1,7 @@
-#include
+#include "3-calc.h"
+/**
+ *
+ *
+ */
+int (*get_op_func(char *s))(int, int)
+
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,1 +1,9 @@
-#include
+#include "3-calc.h"
+/**
+ * main - main function
+ * 
+ * Return: 
+ */
+int main(int argc, char *argv[])
+{
+
