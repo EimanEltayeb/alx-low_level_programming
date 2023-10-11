05520b55b85eedf1acbd33a24b6968d786cd7718
EimanEltayeb <em_moh092@hotmail.com>
2023-10-10 18:17:36 -0700

    fds

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+/**
+ * new_dog - function
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: dog_t
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *new;
+
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
+		return (NULL);
+	else
+	new->name = name;
+	new->age = age;
+	new->owner = owner;
+	return (new);
+}
