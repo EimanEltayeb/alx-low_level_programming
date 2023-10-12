06f030a93299450c095d1564c9bc14652f7bf21b
EimanEltayeb <em_moh092@hotmail.com>
2023-10-12 01:20:39 -0700

    gd

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -17,7 +17,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (num2 == 0)
+	if (num2 == 0 && (argv[2][0] == '/' || argv[2][0] == '%'))
 	{
 		printf("Error\n");
 		exit(100);
