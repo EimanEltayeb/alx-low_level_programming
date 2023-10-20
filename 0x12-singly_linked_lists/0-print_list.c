29a4399c8448aa3e96eafbab8c72a41a6ebcf683
EimanEltayeb <em_moh092@hotmail.com>
2023-10-20 13:41:53 -0700

    g

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * print_list - prints all elements of a list.
+ * @h: input
+ * Return: size_t
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count;
+
+	for (count = 0; h != NULL; count++)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)");
+		else
+			printf("[%u] %s\n",h->len, h->str);
+		h = h->next;
+	}
+	return (count);
+}
