8d13cb77482dbac5fef6cb45d18c6f7452359ca0
EimanEltayeb <em_moh092@hotmail.com>
2023-10-23 21:34:01 -0700

    7

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -9,8 +9,8 @@ int sum_listint(listint_t *head)
 	listint_t *temp;
 	int sum = 0;
 	
-	if (head == NULL)
-		return (0);
+	/*if (head == NULL)*/
+	/*	return (0);*/
 	for (temp = head; temp != NULL; )
 	{
 		sum += temp->n;
