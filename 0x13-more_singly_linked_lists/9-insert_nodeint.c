bf845f32286a5a2b52ae7b900b6c90e7d17e0583
EimanEltayeb <em_moh092@hotmail.com>
2023-10-24 13:35:53 -0700

    gf

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		temp = temp->next;
 	}
-	if (i >= (idx - 1))
+	if (i < (idx - 1))
 	{
 		free(new);
 		return (NULL);
