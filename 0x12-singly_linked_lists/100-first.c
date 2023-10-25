e000059f023fda881874ca7bc95c1ca15f341367
EimanEltayeb <em_moh092@hotmail.com>
2023-10-25 13:37:26 -0700

    gdfv

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include "lists.h"
+/**
+ *before_main - add text before main
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
