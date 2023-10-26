b125ef2aedc642920a3dc8113b73fcb6039d67b7
EimanEltayeb <em_moh092@hotmail.com>
2023-10-26 12:50:53 -0700

    gfdfv

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -9,6 +9,8 @@ int set_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int x;
 
+	if (!n)
+		return (-1);
 	x = 1;
 	x = x << index;
 	*n = *n | x;
---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * flip_bits - returns bits needed to flip to get other num
+ * @n: number
+ * @m: the other num.
+ * @Return: bits to be fliped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
