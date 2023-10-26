02da48509193184adec612ae426ddfd16993df0d
EimanEltayeb <em_moh092@hotmail.com>
2023-10-26 14:53:04 -0700

    gjdkfj

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -3,7 +3,7 @@
  * flip_bits - returns bits needed to flip to get other num
  * @n: number
  * @m: the other num.
- * @Return: bits to be fliped
+ * Return: bits to be fliped
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
