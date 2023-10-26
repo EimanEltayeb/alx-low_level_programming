afbe2a18f636605d2b4d772fcc4e9203072defc8
EimanEltayeb <em_moh092@hotmail.com>
2023-10-26 09:48:52 -0700

    gr

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: the number to be printed
+ */
+void print_binary(unsigned long int n)
+{
+	int i, j, b, x;
+	if (n == 0)
+		_putchar('0');
+	b = sizeof(unsigned long int) * 8;
+	for (i = (b - 1); i >= 0; i--)
+	{
+		x = (n >> i) & 1;
+		if (x)
+			break;
+	}
+	for (j = i; j >= 0; j--)
+	{
+		 x = (n >> j) & 1;
+		_putchar(x + '0');
+	}
+}
---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * get_bit -  returns the value of a bit at a given index.
+ * @n: number
+ * @index: index
+ * Return: number
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int;
