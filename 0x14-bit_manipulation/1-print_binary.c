3c714e492990111718d217ecaa10e1b3fc0133b2
EimanEltayeb <em_moh092@hotmail.com>
2023-10-26 10:52:03 -0700

    fgdf

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -5,15 +5,21 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i, j, b, x;
+	int i, b, x;
+	unsigned int j;
+
 	if (n == 0)
+	{
 		_putchar('0');
+	}
 	b = sizeof(unsigned long int) * 8;
 	for (i = (b - 1); i >= 0; i--)
 	{
 		x = (n >> i) & 1;
 		if (x)
+		{
 			break;
+		}
 	}
 	for (j = i; j >= 0; j--)
 	{
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,4 +1,4 @@
-#include "lists.h"
+#include "main.h"
 /**
  * get_bit -  returns the value of a bit at a given index.
  * @n: number
@@ -7,4 +7,15 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-	int;
+	unsigned int b;
+	int x, i;
+
+	b  = sizeof(unsigned long int) * 8;
+	for (i = (b - 1); i >= 0; i--)
+	{
+		x = (n >> i) & 1;
+		if ((unsigned int)i == index)
+			return (x);
+	}
+	return (-1);
+}
