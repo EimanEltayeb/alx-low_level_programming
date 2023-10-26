db10e956ab5d4cca1853e083207ffc6f8213a9ad
EimanEltayeb <em_moh092@hotmail.com>
2023-10-26 13:23:32 -0700

    hdfhf

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -9,6 +9,8 @@ int clear_bit(unsigned long int *n, unsigned int index)
 {
 	unsigned long int x;
 
+	if (index > (sizeof(unsigned long int) * 8))
+		return (-1);
 	x = 1;
 	x = x << index;
 	x = ~x;
