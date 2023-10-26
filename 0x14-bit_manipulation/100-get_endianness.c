edfc220ff9e9a84d64b2f78d20850fa212e14060
EimanEltayeb <em_moh092@hotmail.com>
2023-10-26 15:25:06 -0700

    advanced:X

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * get_endianness -  function that checks the endianness.
+ * Rreturn: 0 or 1
+ */
+int get_endianness(void)
+{
+	unsigned int n = 1;
+
+	if (*(char *)&n == 1)
+		return (1);
+	else
+		return (0);
+}
