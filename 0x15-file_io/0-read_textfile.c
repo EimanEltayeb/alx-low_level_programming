140c75d2a8c8d8844ba541a8a68a95ed363df815
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 01:35:37 +0300

    g

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -22,11 +22,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (0);
 	readbytes = read(fd, buffer, letters);
 	if (readbytes == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 
 	write_ret =	write(1, buffer, readbytes);
 	if (write_ret == -1)
+	{
+		free(buffer);
+		close(fd);
 		return (0);
+	}
 
 	free(buffer);
 	close(fd);
--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -12,20 +12,24 @@ int create_file(const char *filename, char *text_content)
 	int write_ret;
 	int i;
 
-	for (i = 0; text_content[i] != '\0'; i++)
-	{
-		len++;
-	}
-
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
+
 	if (text_content == NULL)
 	{
 		text_content = "";
 	}
+	else
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+		{
+			len++;
+		}
+	}
+	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	
 	write_ret = write(fd, text_content, len);
 		if (write_ret == -1)
 			return (-1);
