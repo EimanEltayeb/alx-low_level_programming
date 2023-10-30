b9a0d538d7c888288088908f7b1bea80be335d7d
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 01:13:29 +0300

    gk

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	fd = open(filename, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	write_ret = write(1, text_content, len);
+	write_ret = write(fd, text_content, len);
 		if (write_ret == -1)
 			return (-1);
 	close(fd);
