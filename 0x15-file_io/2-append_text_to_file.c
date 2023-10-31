db42af8f39f2b025e24f11b285f49d323a6c0553
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 03:00:12 +0300

    g

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -7,7 +7,7 @@
 */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int write_ret, len, fd, fd2, i;
+	int write_ret, len = 0, fd, fd2, i;
 
 	if (filename == NULL)
 		return (-1);
