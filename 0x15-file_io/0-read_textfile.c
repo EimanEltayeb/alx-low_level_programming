971a0f38f50460c8c59df17fcad465f390f994cf
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 05:43:30 +0300

    f

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -25,7 +25,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	write_ret =	write(1, buffer, readbytes);
-	if (write_ret == -1)
+	if (write_ret == -1 || write_ret != (int)readbytes)
 	{
 		close(fd);
 		return (0);
