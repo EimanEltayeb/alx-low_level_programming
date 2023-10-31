a475c814f0888ffe0f53098d267632c9c0c2dde0
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 03:18:59 +0300

    g

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,22 +8,18 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	char *buffer;
 	ssize_t readbytes;
 	int write_ret;
+	char buffer[BUF_SIZE * 8];
 
 	if (filename == NULL)
 		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buffer = malloc(sizeof(char) * letters * 1024 * 8);
-	if (buffer == NULL)
-		return (0);
 	readbytes = read(fd, buffer, letters);
 	if (readbytes == -1)
 	{
-		free(buffer);
 		close(fd);
 		return (0);
 	}
@@ -31,12 +27,9 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	write_ret =	write(1, buffer, readbytes);
 	if (write_ret == -1)
 	{
-		free(buffer);
 		close(fd);
 		return (0);
 	}
-
-	free(buffer);
 	close(fd);
 	return (readbytes);
 }
