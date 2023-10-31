bdce6a772b9c8db593f062bb5f5cdf5e68503cc3
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 05:01:39 +0300

    g

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,7 +7,7 @@
 */
 int main(int argc, char **argv)
 {
-	int fd, fd_to;
+	int fd, fd_to, x, y;
 	char buffer[BUF_SIZE];
 	ssize_t write_bytes, read_bytes;
 
@@ -22,7 +22,7 @@ int main(int argc, char **argv)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to %s\n", argv[2]);
@@ -37,9 +37,9 @@ int main(int argc, char **argv)
 			exit(99);
 		}
 	}
-	close(fd);
-	close(fd_to);
-	if (close(fd == -1) || close(fd_to) == -1)
+	x = close(fd);
+	y = close(fd_to);
+	if (x == -1 || y == -1)
 	{
 		dprintf(2, "Error: Can't close fd FD_VALUE\n");
 		exit(100);
