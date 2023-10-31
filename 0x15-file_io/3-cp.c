a14478788c9f787b1257deeed727b8e29bb23eda
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 04:41:59 +0300

    f

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,16 @@
 #include "main.h"
 /**
  * main - maint function
+ * @argc: num of arguments
+ * @argv: the arguments
  * Return: 0;
 */
 int main(int argc, char **argv)
 {
 	int fd, fd_to;
-	char buffer[BUF_SIZE * 8];
-	
+	char buffer[BUF_SIZE];
+	ssize_t write_bytes, read_bytes;
+
 	if (argc != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
@@ -20,11 +23,27 @@ int main(int argc, char **argv)
 		dprintf(2, "Error: Can't read from file NAME_OF_THE_FILE\n");
 		exit(98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	fd_to = open(argv[2], O_WRONLY | O_TRUNC | O_CREAT, 0664);
 	if (fd_to == -1)
 	{
 		dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
 		exit(99);
 	}
-
+	while ((read_bytes = read(fd, buffer, BUF_SIZE)) > 0)
+	{
+		write_bytes = write(fd_to, buffer, read_bytes);
+		if (write_bytes == -1)
+		{
+			dprintf(2, "Error: Can't write to NAME_OF_THE_FILE\n");
+			exit(99);
+		}
+	}
+	close(fd);
+	close(fd_to);
+	if (close(fd == -1) || close(fd_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd FD_VALUE\n");
+		exit(100);
+	}
+	return (0);
 }
