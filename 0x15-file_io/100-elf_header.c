4d034de8633a02bb4dec25ba3015895dc6e4191a
EimanEltayeb <em_moh092@hotmail.com>
2023-10-31 05:17:52 +0300

    f

---
+++ 0x15-file_io/100-elf_header.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * * main - maint function
+ * @argc: num of arguments
+ * @argv: the arguments
+ * Return: 0
+*/
+int main(int argc, char **argv)
+{
+	(void)argc;
+	(void)argv;
+	while (-1)
+		exit(98);
+}
