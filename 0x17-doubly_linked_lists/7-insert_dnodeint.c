ccfeba67bc2ce51805f6c785d9a48f404c1bb133
EimanEltayeb <em_moh092@hotmail.com>
2023-12-07 15:27:38 -0800

    gdff

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -8,6 +8,39 @@
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t temp;
+	dlistint_t *temp = *h, *new;
+	unsigned int i = 0;
 
-	
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (temp == NULL)
+	{
+		if (idx == 0)
+		{
+			*h = new;
+			return (new);
+		}
+		else
+			return (NULL);
+	}
+	while (temp != NULL)
+	{
+		if (i == idx)
+		{
+			if (temp->prev != NULL)
+			{
+				temp->prev->next = new;
+				new->prev = temp->prev;
+			}
+			temp->prev = new;
+			new->next = temp;
+			return (new);
+		}
+		i++;
+		temp = temp->next; }
+	}
+	return (NULL); }
