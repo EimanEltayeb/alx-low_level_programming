902a8230309dc9b9113a40c2c83292ac67fc9b24
EimanEtltayeb <em_moh092@hotmail.com>
2023-12-07 05:27:45 -0800

    gdg

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -1,6 +1,8 @@
 #include "lists.h"
 /**
- *
+ * print_dlistint - prints elements of dlistint_t
+ * @h: header pointer of the list
+ * Return: size of the list
  */
 size_t print_dlistint(const dlistint_t *h)
 {
