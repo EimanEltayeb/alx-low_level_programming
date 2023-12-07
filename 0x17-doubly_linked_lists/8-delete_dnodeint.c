a892195589bf7f12ca56e6a3461c04e7611eaada
EimanEltayeb <em_moh092@hotmail.com>
2023-12-07 13:55:23 -0800

    gdfg

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * delete_dnodeint_at_index - delete node at index.
+ * @head: pointer
+ * @index: index of the node to be deleted
+ * Retern 1 or -1
+ */
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int i = 0;
+
+	if (index = 0)
+	{
+		temp = (*head)->next;
+		temp->prev = NULL;
+		free(*head);
+		*head = temp;
+		return (1);
+	}	
+	while (temp != NULL)
+	{
+		if (i == index)
+		{
+			if (temp->next != NULL)
+				temp->next->prev = temp->prev;
+			temp->prev->next = temp->next;
+			free(temp);
+			return (1)
+		}
+		else
+		{
+			i++;
+			temp = temp->next;
+		}
+	}
+	return (-1)
+}
