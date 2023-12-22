96c826218b935bc01aca651bd10775ab1293868e
EimanEltayeb <em_moh092@hotmail.com>
2023-12-23 02:20:22 +0300

    d

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set - adds elements to the hash table
+ * @ht: hash table
+ * @key: the key
+ * @value: the value
+ * Return: 0 if failed or 1 if succeeded
+*/
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index, size;
+	hash_node_t *temp, *new;
+
+
+	size = ht->size;
+	index = key_index((const unsigned char *)key, size);
+
+	if (key == NULL)
+		return (0);
+
+	new = malloc(sizeof(hash_node_t *));
+	if (new == NULL)
+		return (0);
+
+	new->key = strdup(key);
+	new->value = strdup(value);
+	new->next = NULL;
+
+	if (ht->array[index] == NULL)
+		new = ht->array[index];
+	else
+	{
+		temp = ht->array[index];
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = new;
+	}
+	return (1);
+}
