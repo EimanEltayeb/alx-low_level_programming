95355a4165d2e7244530e3c3e14163d59f5576e3
EimanEltayeb <em_moh092@hotmail.com>
2023-12-22 18:18:43 +0300

    d

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -8,12 +8,7 @@
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
 	unsigned long int x;
-	/*hash_table_t *h_table;*/
-
-	/*h_table = hash_table_create(size);*/
 	
 	x = hash_djb2(key);
-	if (x >= size)
-		return (x);
-	return (x % 237);
+	return (x % size);
 }
