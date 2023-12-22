827c167984c041812d4b944ff061cfaec36bbfb3
EimanEltayeb <em_moh092@hotmail.com>
2023-12-22 09:30:06 +0300

    d

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -7,14 +7,12 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table;
-	unsigned long i;
 
 	h_table = malloc(sizeof(hash_table_t));
 	if (h_table == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
-		
-	h_table = NULL;
+	h_table->size = size;
+	h_table->array = malloc(sizeof(hash_table_t) * size);
 	
 	return (h_table);
 }
