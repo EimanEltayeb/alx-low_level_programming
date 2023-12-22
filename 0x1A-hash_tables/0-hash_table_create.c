adadb0ff69da23d82782306fb86957224af68623
EimanEltayeb <EimanEltayeb>
2023-12-22 05:26:42 +0300

    d

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,23 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates a hash table
+ * @size: size of the array
+ * Return: pointer to the hash table
+*/
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t **h_table, *h;
+
+	unsigned long int i;
+
+	h_table = malloc(sizeof(hash_table_t*) * size);
+	if (h_table == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		h_table[i] = NULL;
+	}
+	h = *h_table;
+	return (h);
+}
