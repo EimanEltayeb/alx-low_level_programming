c351edd8879ae2596af6dc03a2fdad43d345a2d0
EimanEltayeb <em_moh092@hotmail.com>
2023-12-23 04:07:18 +0300

    betty fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -8,13 +8,12 @@
 */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index, size;
+	unsigned long int index;
 	hash_node_t *temp, *new;
 
 	if (ht == NULL || key == NULL)
 		return (0);
-	size = ht->size;
-	index = key_index((const unsigned char *)key, size);
+	index = key_index((const unsigned char *)key, ht->size);
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
 		return (0);
@@ -22,15 +21,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (new->key == NULL)
 	{
 		free(new);
-		return (0);
-	}
+		return (0); }
 	new->value = strdup(value);
 	if (new->key == NULL)
 	{
 		free(new->key);
 		free(new);
-		return (0);
-	}
+		return (0); }
 	new->next = NULL;
 	if (ht->array[index] == NULL)
 		ht->array[index] = new;
@@ -46,13 +43,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				free(new->key);
 				free(new->value);
 				free(new);
-				return (1);
-			}
+				return (1); }
 			if (temp->next == NULL)
 				break;
-			temp = temp->next;
-		}
-		temp->next = new;
-	}
-	return (1);
-}
+			temp = temp->next; }
+		temp->next = new; }
+	return (1); }
