0ead6ee5101973039298e68bfd88a05be8899497
EimanEltayeb <em_moh092@hotmail.com>
2023-12-23 05:47:43 +0300

    delet

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_delete - delete a hash table
+ * @ht: hash table to be deleted
+*/
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *temp1, *temp2;
+
+	if (ht != NULL)
+	{
+		for (index = 0; index < ht->size; index++)
+		{
+			if (ht->array[index] == NULL)
+				continue;
+			temp1 = ht->array[index];
+			while (temp1)
+			{
+				temp2 = temp1->next;
+				if (temp1->key != NULL)
+					free(temp1->key);
+				if (temp1->value != NULL)
+					free(temp1->value);
+				if (temp1 != NULL)
+					free(temp1);
+				temp1 = temp2;
+			}
+		}
+	}
+}
