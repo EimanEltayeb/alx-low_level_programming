f701c04dcf6ea32e6d18962a3ddf0ff8d87f2dc7
EimanEltayeb <em_moh092@hotmail.com>
2023-12-23 04:40:32 +0300

    d

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table
+ * @ht: hash table to be printed
+*/
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int index;
+	hash_node_t *temp;
+
+	if (ht)
+	{
+		printf("{");
+		for (index = 0; index < ht->size; index ++)
+		{
+			if (ht->array[index] == NULL)
+				{
+					index++;
+					continue;
+				}
+			temp = ht->array[index];
+			while (temp)
+			{
+				printf("'%s': '%s'", temp->key, temp->value);
+				temp = temp->next;
+			}
+			if (index + 2 != ht->size)
+				printf(", ");
+		}
+		printf("}\n");
+	}
+}
