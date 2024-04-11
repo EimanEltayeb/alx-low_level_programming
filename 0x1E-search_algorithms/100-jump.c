a5dbfaba28b12a0e501d769ecadc68894c11d859
EimanEltayeb <em_moh092@hotmail.com>
2024-04-11 03:02:57 -0700

    hfgh

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,34 @@
+#include "search_algos.h"
+/**
+ * jump_search - jump search algorithm
+ * @array: array to be searched
+ * @size: size of the array
+ * @value: value to search for
+ * Return: index of value if found
+ */
+int jump_search(int *array, size_t size, int value)
+{
+	size_t m, l, h;
+
+	m = sqrt(size);
+	if (!array)
+		return (-1);
+	for (l = 0, h = m; h < size; l += m, h+= m)
+	{
+		printf("Value checked array[%ld] = [%d]\n", l, array[l]);
+		if (array[h] < value)
+			continue;
+		if (array[h] >= value || (h + m >= size))
+		{
+			printf("Value found between indexes [%ld] and [%ld]\n", l, h);
+			break;
+		}
+	}
+	for (; l <= h; l++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", l, array[l]);
+		if (array[l] == value)
+			return (l);
+	}
+	return (-1);
+}
