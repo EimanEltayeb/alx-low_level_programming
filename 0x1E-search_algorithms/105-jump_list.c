5c40a0167460ecaf9a2c79a41553d81bb8eb1aba
EimanEltayeb <em_moh092@hotmail.com>
2024-04-11 19:46:27 -0700

    gfdgdf

---
+++ 0x1E-search_algorithms/105-jump_list.c
@@ -0,0 +1,13 @@
+#include "search_algos.h"
+/**
+ * jump_list - jump list searching
+ * @list: the list
+ * @size: size of list
+ * @value: value to be searched for
+ * Return: pointer to the node
+ */
+listint_t *jump_list(listint_t *list, size_t size, int value)
+{
+	if (!list)
+		return (-1);
+}
